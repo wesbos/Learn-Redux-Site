12e95cab9a68ca494a6826b2fcea266a1786a6db
wesbos <wesbos@gmail.com>
2016-05-06 12:32:26 -0400

    start with flexbox.io site

---
+++ Gulpfile.js
@@ -0,0 +1,136 @@
+/*
+  first we load in gulp
+*/
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+var babelify = require('babelify');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var gutil = require('gulp-util');
+var historyApiFallback = require('connect-history-api-fallback');
+
+/*
+  then we load all plugins into the p variable with gulp-load-plugins
+  we do this so that we don't have to load each one individually
+  you will see many examples online that call concat(), sass() or uglify()
+  These are now just p.concat(), p.sass() or p.uglify()
+*/
+var p = require('gulp-load-plugins')();
+
+// globs are basically pattern matches for files. We store them in an object
+// because we use them both for gulp.src() as well as gulp.watch()
+// if a change to the glob is needed, we simply edit it here rather than in 2 places
+var globs = {
+  "scripts" : ['source/js/*.js'],
+  "styles"  : ['source/css/style.styl'],
+  "templates"  : ['source/**/*.jade', '!source/**/layout.jade'],
+  "images"  : ['source/images/**/*'],
+}
+
+gulp.task('scripts',function() {
+
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './source/js/viewer.js',
+    debug: true
+  }).transform(babelify, {presets: ["es2015", "react"]});
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(p.plumber({errorHandler: p.notify.onError("Error: <%= error.message %>")}))
+    .pipe(p.sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        // .pipe(p.uglify())
+        .on('error', gutil.log)
+    .pipe(p.sourcemaps.write('./'))
+    .pipe(gulp.dest('./_build/js/'))
+    .pipe(reload({stream:true}))
+    .pipe(p.notify({
+      title : 'Scripts Compiled!',
+      sound:false
+    }))
+});
+
+gulp.task('styles',function() {
+  gulp.src(globs.styles , { base : 'source/css/' })
+    .pipe(p.plumber({errorHandler: p.notify.onError("Error: <%= error.message %>")}))
+    .pipe(p.stylus())
+    .pipe(p.autoprefixer())
+    .pipe(gulp.dest('./_build/css/'))
+    .pipe(reload({stream:true}))
+    .pipe(p.notify({
+      title : 'Styles Done!!',
+      sound : true,
+      message : 'ya man'
+    }))
+});
+
+gulp.task('templates',function() {
+  gulp.src(globs.templates, { base : 'source/' })
+    .pipe(p.jade({pretty: true}))
+    .pipe(p.debug())
+    .pipe(gulp.dest('./_build/'))
+    .pipe(reload({stream:true}))
+});
+
+gulp.task('images',function() {
+  gulp.src(globs.images, { base : 'source/images' })
+      .pipe(p.debug())
+      .pipe(p.imagemin({
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}]
+      }))
+      .pipe(gulp.dest('_build/images'))
+});
+
+gulp.task('fonts',function() {
+  gulp.src('source/css/fonts/**/*', { base : 'source/css/fonts' })
+  .pipe(gulp.dest('_build/css/fonts'))
+});
+
+gulp.task('htaccess',function() {
+  gulp.src('source/.htaccess', { base : 'source/' })
+    .pipe(gulp.dest('_build/'))
+});
+
+// Start the server
+gulp.task('browser-sync', function() {
+    browserSync({
+        open : false,
+        middleware : [ historyApiFallback() ],
+        server: {
+            baseDir: "./_build"
+        }
+    });
+});
+
+gulp.task('clean',function() {
+  // we return it so it runs async
+  return gulp.src('_build/',{read:false})
+    .pipe(p.clean({ force : true }))
+});
+
+gulp.task('watch', ['browser-sync'] ,function() {
+  gulp.watch(globs.scripts,['scripts']);
+  gulp.watch(globs.styles,['styles']);
+  gulp.watch(globs.templates,['templates']);
+});
+
+
+gulp.task('deploy', function() {
+  gulp.src('_build/**/*', { base : '_build/' })
+    .pipe(p.sftp({
+        host: 'bostype.com',
+        user : 'bostypec',
+        remotePath : '/home3/bostypec/www/flexbox'
+    }));
+});
+
+// the default tasks runs when you simply type 'gulp'
+gulp.task('default',['styles','fonts','scripts','images','templates','htaccess','watch']);
---
+++ source/js/viewer.js
@@ -0,0 +1,208 @@
+import React from 'react';
+import { render } from 'react-dom'
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
+import polyfill from './polyfill';
+
+polyfill();
+
+// cowboy in my previous jQuery
+const scripts = require('./scripts')();
+const data = require('./data');
+
+/*
+  Videos Component for displaying a list of all videos
+*/
+const Videos = React.createClass({
+  render : function() {
+    return (
+      <div className="videos">
+        {this.props.videos.map((vid, i) => {
+          return <Video {...this.props} vid={vid} i={i} key={i}/>
+        })}
+      </div>
+    )
+  }
+});
+
+
+/*
+  Listed Video Component
+*/
+const Video = React.createClass({
+  updateHash : function(e) {
+    e.preventDefault();
+    const { vid } = this.props;
+
+    // check if they have "authenticated"
+    if(!!localStorage.hash) {
+      // TODO: Account for these old urls
+      // window.location.hash = "#/view/" + this.props.vid.id;
+      this.context.router.push(`/view/${vid.id}`);
+      $("html, body").animate({ scrollTop: $('#watch').position().top });
+    } else  {
+      $("html, body").animate({ scrollTop: $('.signup').position().top }, 1000);
+      $('input[name=email]').focus();
+      $('p.underline').text('Looks like you aren\'t logged in. Lost your link? Enter your email again and I\'ll re-send the link.').show();
+    }
+  },
+  render : function() {
+    var video = this.props.vid;
+    return (
+      <div className="video">
+        <span className="video-number">{this.props.i +  1}</span>
+         <a onClick={this.updateHash}>
+           <img width="100" height="56.25" src={"https://img.youtube.com/vi/" + video.id + "/mqdefault.jpg"} alt={video.description} />
+            <h2>
+              {video.title}
+              <span className="duration">{video.duration}</span>
+            </h2>
+           <p className="description">{video.description}</p>
+          </a>
+      </div>
+    )
+  },
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  }
+});
+
+/*
+  Viewer Component
+*/
+
+const Viewer = React.createClass({
+
+  getInitialState: function() {
+    var params = this.props.params;
+    return {
+       videos : data.videos,
+       video : data.videos[0],
+       videoid : params.videoid || data.videos[0].id
+    }
+  },
+  // This gets called right before render - we use it to set some state
+  componentWillMount : function() {
+    // console.log("viewer updating..");
+  },
+  goToVideo : function(videoid, e) {
+    e.preventDefault();
+    this.context.router.push(`/view/${videoid}`);
+    this.setState({videoid : videoid});
+  },
+  renderNavButtons : function() {
+    var params = this.props.params;
+    var videoid = this.state.videoid;
+
+    // var index = findIndex(this.state.videos,{id : this.state.videoid});
+    var index = this.state.videos.findIndex(video => video.id === this.state.videid);
+
+    var prevVideo = this.state.videos[index - 1];
+    var prevButton = (prevVideo ? <a onClick={this.goToVideo.bind(null,prevVideo.id)} href={`#/view/${prevVideo.id}`}>← {prevVideo.title}</a> : '');
+
+    var nextVideo = this.state.videos[index + 1];
+    var nextButton = (nextVideo ? <a onClick={this.goToVideo.bind(null,nextVideo.id)} href={`#/view/${nextVideo.id}`}>{nextVideo.title} → </a> : '');
+
+    return (
+      <nav className="prev-and-next">
+        {prevButton}
+        {nextButton}
+      </nav>
+    )
+  },
+  render : function() {
+    var params = this.props.params;
+    var videoid = this.state.videoid;
+    var video = this.state.videos.find(video => video.id === videoid)
+    var autoplay = (videoid === this.state.videos[0].id ? 0 : 1);
+
+    return (
+      <div className="viewer">
+        <Player videoid={videoid} autoplay={autoplay}/>
+        <div className="video-details">
+          <h2>Now Playing: {video.title} - {video.number} of 20</h2>
+          <p>{video.description}</p>
+          {this.renderNavButtons()}
+        </div>
+        <Videos {...this.props} videos={this.state.videos} />
+      </div>
+
+    )
+  },
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  }
+});
+
+/*
+  Video Player
+*/
+
+var Player = React.createClass({
+  generateURL : function() {
+    // sorry about the whistling
+    return `https://www.youtube.com/embed/${this.props.videoid}?autoplay=${this.props.autoplay}&showinfo=0&rel=0&start=9`;
+  },
+  render : function() {
+    return (
+      <div className="scaler">
+        <iframe width="560" height="315" src={this.generateURL()} frameBorder="0" allowFullScreen></iframe>
+      </div>
+    )
+  }
+});
+
+/*
+  Authentication
+*/
+
+const Auth = React.createClass({
+  getInitialState : function() {
+    return {
+      videos : data.videos
+    }
+  },
+  componentWillMount() {
+    localStorage.hash = this.props.params.hash;
+    this.context.router.push(`/view/${this.state.videos[0].id}`);
+  },
+  render() {
+    return <div></div>
+  },
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  }
+});
+
+const Main = React.createClass({
+  // View the key prop so the viewer will re-render on route update
+  render() {
+    return <div>{React.cloneElement(this.props.children, { key: this.props.params.videoid })}</div>
+  },
+  componentWillMount() {
+    var oldURLs = new RegExp(/auth|view/, 'gi');
+    if(window.location.hash && window.location.hash.match(oldURLs)) {
+      const url = window.location.hash.replace('#','');
+      this.context.router.push(`${url}`);
+    }
+  },
+  contextTypes: {
+    location: React.PropTypes.object.isRequired,
+    router: React.PropTypes.object.isRequired
+  }
+});
+
+/*
+  Router
+*/
+
+var routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <Route name="auth" path="auth/:hash" component={Auth} />
+      <Route name="view" path="view/:videoid" component={Viewer}></Route>
+      <IndexRoute component={Viewer} />
+    </Route>
+  </Router>
+);
+
+render(routes, document.getElementById('watch'));
